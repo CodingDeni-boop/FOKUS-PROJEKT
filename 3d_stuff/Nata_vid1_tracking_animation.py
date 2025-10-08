6286061f3df694e34a10b6d977116045a8e6e084
nata <natalie.haldner@gmail.com>
2025-10-08 13:29:52 +0200

    tracking animation :D

--- 3d_stuff/Nata_vid1_tracking_animation.py
+++ 3d_stuff/Nata_vid1_tracking_animation.py
@@ -27,7 +27,7 @@ test3dcol_tri.rescale_by_known_distance("tl","br", 0.64, dims = ("x","y","z"))
 test3dcol_tri.plot(trajectories=["nose"], static=["tl", "tr", "br", "bl"], lines = [("tl", "tr")], dims=("x", "y", "z"))
 
 ### Skeleton Tracking Animation
-test3dcol_tri.save_3d_tracking_video_multi_view(out_path = "./Nata_Animation_Output/vid_1_anim.gif", startframe = 1, endframe = 500, lines = [("nose", "neck"),()])
+test3dcol_tri.save_3d_tracking_video_multi_view(out_path = "./Nata_Animation_Output/vid_1_anim.gif", startframe = 1, endframe = 500, lines = [("tl","tr"),("tr","br"),("br","bl"),("bl","tl"),("nose", "neck"),("tailbase","tailcentre"),("tailcentre","tailtip")])
 
 #ffmpeg not recognized
 #test3dcol_tri.save_3d_tracking_video_multi_view(out_path = "./Nata_Animation_Output/vid_1_anim.mp4", startframe = 1, endframe = 500, lines = [("nose", "neck")], writer ="ffmpeg")
