ff208d9ffad45ca76ab7b98d35e50c06a6777074
CodingDeni-boop <dbugatti@ethz.ch>
2025-10-08 09:47:26 +0200

    esr5xd6ctf7yg8u

--- 3d_stuff/Nata_3dr.py
+++ 3d_stuff/Nata_3dr.py
@@ -1,7 +1,7 @@
 import py3r.behaviour as bv
 
 # load a folder of tracking files as a collection
-tc = bv.TrackingCollection.from_dlc_folder('path/to/folder', options=opts)
+tc = bv.TrackingCollection.from_dlc_folder('path/to/folder')
 
 # generate features for all animals
 fc = bv.FeaturesCollection.from_tracking_collection(tc)
