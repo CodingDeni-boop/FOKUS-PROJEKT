12ab792bab63d0907d67efc751c59f926302bf5a
CodingDeni-boop <dbugatti@ethz.ch>
2025-10-09 18:59:22 +0200

    restructuring directory

--- 3d_stuff/Nata_3dr.py
+++
@@ -1,7 +0,0 @@
-import py3r.behaviour as bv
-
-# load a folder of tracking files as a collection
-tc = bv.TrackingCollection.from_dlc_folder('path/to/folder')
-
-# generate features for all animals
-fc = bv.FeaturesCollection.from_tracking_collection(tc)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from Random_tools import tools
 
 tools.drop_secundary_columns(collection_folder_path="3d_stuff/oft_tracking/Empty_Cage/collection/")
+
+
