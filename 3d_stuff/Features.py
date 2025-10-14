bc9c110ea67014def2ac13d0a9aa10c44e324bda
nata <natalie.haldner@gmail.com>
2025-10-14 13:26:37 +0200

    -

--- 3d_stuff/Features.py
+++ 3d_stuff/Features.py
@@ -9,7 +9,6 @@ from py3r.behaviour.features.features_result import FeaturesResult
 from py3r.behaviour.tracking.tracking_collection import TrackingCollection as Collection
 from py3r.behaviour.tracking.tracking_mv import TrackingMV as MultiView
 
-collection_folder_path_src = "oft_tracking/Empty_Cage/collection/"
 options = opt(fps=30)
 
 tracking_collection = Collection.from_yolo3r_folder("./oft_tracking/Empty_Cage/collection",options,MultiView)
@@ -24,10 +23,17 @@ fc.azimuth('nose','neck').store()
 fc.azimuth('neck', 'bodycentre').store()
 fc.azimuth('headcentre', 'neck').store()
 
-# Speed
-
-
-
-
 # Distance
 
+pairs_of_points = pd.DataFrame({
+    "point1": ["nose", "nose", "neck", "neck", "neck", "neck", "bcl", "bcr", "hipl", "hipr"],
+    "point2": ["earl", "earr", "earl", "earr", "bcl", "bcr", "hipl", "hipr", "tailbase", "tailbase"]
+})
+
+for i in range(0,pairs_of_points.shape[0]):
+    fc.distance_between(pairs_of_points.iloc[i,0],pairs_of_points.iloc[i,1],dims=("x","y","z")).store()
+print(fc[0].data)
+
+# Speed
+
+#Area of Mouse
