2ddb9bf5e50f442d9226edf791f7133f08545525
nata <natalie.haldner@gmail.com>
2025-10-14 13:22:54 +0200

    -

--- 3d_stuff/Features.py
+++ 3d_stuff/Features.py
@@ -6,10 +6,28 @@ from py3r.behaviour.tracking.tracking_mv import TrackingMV as mv
 from py3r.behaviour.features.features import Features
 from py3r.behaviour.features.features_collection import FeaturesCollection
 from py3r.behaviour.features.features_result import FeaturesResult
+from py3r.behaviour.tracking.tracking_collection import TrackingCollection as Collection
+from py3r.behaviour.tracking.tracking_mv import TrackingMV as MultiView
 
+collection_folder_path_src = "oft_tracking/Empty_Cage/collection/"
+options = opt(fps=30)
 
-fc = FeaturesCollection.from_tracking_collection(test3dcol_tri)
+tracking_collection = Collection.from_yolo3r_folder("./oft_tracking/Empty_Cage/collection",options,MultiView)
+triangulated_tracking_collection = tracking_collection.stereo_triangulate()
+triangulated_tracking_collection.strip_column_names()
+triangulated_tracking_collection.rescale_by_known_distance("hipl","hipr", 0.05, dims = ("x","y","z"))
+
+fc = FeaturesCollection.from_tracking_collection(triangulated_tracking_collection)
 
 # Azimuth
+fc.azimuth('nose','neck').store()
+fc.azimuth('neck', 'bodycentre').store()
+fc.azimuth('headcentre', 'neck').store()
 
+# Speed
+
+
+
+
+# Distance
 
--- 3d_stuff/RandomForestClassifier.py
+++ 3d_stuff/RandomForestClassifier.py
@@ -9,8 +9,15 @@ from sklearn.metrics import classification_report
 import numpy as np
 from Nata_features import *
 
+# Extract features
+feature_dict = {}
+for file in fc.keys():
+    feature_obj = fc[file].data
+    feature_dict[file] = feature_obj
+features_obj = fc.features_dict['video_1_3dset']
 
 labels_vid1 = labels_vid1.iloc[1:] # delete frame 0 row
+
 """
 y_cat = pd.Categorical(labels_vid1)
 print(f"Categories: {y_cat.categories}")
