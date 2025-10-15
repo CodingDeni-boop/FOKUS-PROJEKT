3938bbf45d29b80ee67d0ec22e22c73d5acadab6
CodingDeni-boop <dbugatti@ethz.ch>
2025-10-15 09:16:12 +0200

    no ds stores

--- 3d_stuff/Dani_collection_triangulator.py
+++ 3d_stuff/Dani_collection_triangulator.py
@@ -1,37 +1,55 @@
-import os
-import shutil
 import pandas as pd
-import numpy as np
 import py3r.behaviour as py3r
 from py3r.behaviour.tracking.tracking import LoadOptions as opt
-from py3r.behaviour.tracking.tracking_collection import TrackingCollection 
-from py3r.behaviour.tracking.tracking_mv import TrackingMV
+import json
+from py3r.behaviour.features.features import Features
 from py3r.behaviour.features.features_collection import FeaturesCollection
+from py3r.behaviour.features.features_result import FeaturesResult
+from py3r.behaviour.tracking.tracking_collection import TrackingCollection
+from py3r.behaviour.tracking.tracking_mv import TrackingMV
 
 options = opt(fps=30)
 
-"""
-for file in os.listdir(collection_folder_path_src): 
-    left = pd.read_csv(collection_folder_path_src+file+"/left.csv")
-    right = pd.read_csv(collection_folder_path_src+file+"/right.csv")
-    print(file,left.shape, right.shape)
-"""
-tracking_collection = TrackingCollection.from_yolo3r_folder("./oft_tracking/Empty_Cage/collection",options,TrackingMV)
+tracking_collection = TrackingCollection.from_yolo3r_folder("./oft_tracking/Empty_Cage/collection",options, TrackingMV)
 triangulated_tracking_collection = tracking_collection.stereo_triangulate()
 triangulated_tracking_collection.strip_column_names()
 triangulated_tracking_collection.rescale_by_known_distance("hipl","hipr", 0.05, dims = ("x","y","z"))
 
-"""
-triangulated_tracking_collection.plot(trajectories=["bodycentre"], dims=("x", "y", "z"))
-"""
+fc = FeaturesCollection.from_tracking_collection(triangulated_tracking_collection)
+
+# Azimuth
+fc.azimuth('nose','neck').store()
+fc.azimuth('neck', 'bodycentre').store()
+fc.azimuth('headcentre', 'neck').store()
+
+# Distance
 
 pairs_of_points = pd.DataFrame({
     "point1": ["nose", "nose", "neck", "neck", "neck", "neck", "bcl", "bcr", "hipl", "hipr"],
     "point2": ["earl", "earr", "earl", "earr", "bcl", "bcr", "hipl", "hipr", "tailbase", "tailbase"]
 })
 
-collection = FeaturesCollection.from_tracking_collection(triangulated_tracking_collection)
 for i in range(0,pairs_of_points.shape[0]):
-    collection.distance_between(pairs_of_points.iloc[i,0],pairs_of_points.iloc[i,1],dims=("x","y","z")).store()
-print(collection[0].data)
+    fc.distance_between(pairs_of_points.iloc[i,0],pairs_of_points.iloc[i,1],dims=("x","y","z")).store()
+print(fc[0].data)
 
+# Speed
+
+videos = []
+videos.append(file)
+
+#Area of Mouse
+
+embedding_dfs = {
+    name: f.embedding_df(embedding_dict)
+    for name, f in self.features_dict.items()
+}
+# Check all embeddings have the same columns
+columns = next(iter(embedding_dfs.values())).columns
+if not all(df.columns.equals(columns) for df in embedding_dfs.values()):
+    raise ValueError("All embeddings must have the same columns")
+
+# Concatenate with keys to create a MultiIndex
+combined = pd.concat(embedding_dfs.values(), axis=0, keys=embedding_dfs.keys())
+valid_mask = combined.notna().all(axis=1)
+valid_combined = combined[valid_mask]
