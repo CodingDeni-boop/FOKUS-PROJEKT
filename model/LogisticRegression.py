c2a163691e856d1791594b516e2dab951cc05b7e
nata <natalie.haldner@gmail.com>
2025-10-22 09:27:42 +0200

    Log reg

--- model/LogisticRegression.py
+++ model/LogisticRegression.py
@@ -12,51 +12,15 @@ from FeatureSelection import L2_regularization
 import time
 from sklearn.linear_model import LogisticRegression
 from Prepare_Data import load_and_prepare_data
+from sklearn.model_selection import GridSearchCV
+
 
 start=time.time()
 
+################################## Load Data ###########################################################################
+
 X_train, X_test, y_train, y_test = load_and_prepare_data()
 
-"""
-
-y = pd.read_csv("nataliia_labels.csv", index_col=["video_id","frame"])
-X = pd.read_csv("features.csv", index_col=["video_id","frame"])
-X = drop_non_analyzed_videos(X,y)
-X, y = drop_last_frame(X,y)
-
-print("X shape", X.shape)
-
-######################################### MISSING DATA ###########################################
-
-na_percentage = X.isna().mean()
-columns_to_keep = na_percentage[na_percentage <= 0.1].index
-columns_dropped = na_percentage[na_percentage > 0.1].index
-
-print(f"Dropped {len(columns_dropped)} columns with >10% missing values:")
-print(columns_dropped.tolist())
-X = X[columns_to_keep]
-
-valid_mask = X.notna().all(axis=1)
-valid_X = X[valid_mask]
-valid_y = y[valid_mask]
-
-####################################### Train/Test Split ##########################################
-X_train, X_test, y_train, y_test = video_train_test_split(
-    valid_X, valid_y, test_videos=2)
-
-# Convert labels to numpy arrays
-y_train = y_train.values.ravel()
-y_test = y_test.values.ravel()
-
-#######################  SCALING ####################
-from sklearn.preprocessing import StandardScaler
-
-num_features = X_train.select_dtypes(include=[np.number]).columns.tolist()
-
-sc = StandardScaler()
-X_train[num_features] = sc.fit_transform(X_train[num_features])
-X_test[num_features] = sc.transform(X_test[num_features])
-"""
 
 ################################ Basic Model ###########################################################################
 """
@@ -66,16 +30,94 @@ lr.fit(X_train, y_train)
 evaluate_model(lr, X_train, y_train, X_test, y_test)
 """
 
-########################################## Feature Selection ################################################
+################################### Feature Selection ##################################################################
 
-# Regularization
+#L1 REGULARIZATION (can recucde to 0)
+def L1_regularization(lr, X_train, y_train, X_test, y_test):
+    param_grid = {
+        "C": [0.01, 0.1, 1.0],  # Regularization strength
+        "penalty": ["l1"],  # L1 regularization
+        "solver": ["saga"]  # Solver for logistic regression / also possible; ovr
+    }
+    LR_L1 = GridSearchCV(
+        lr,
+        param_grid,
+        cv=5 ,
+        scoring ='f1_weighted'
+    )
+
+    # Fit GridSearchCV to training data
+    LR_L1.fit(X_train, y_train)
+
+    # Best L1 model after hyperparameter tuning
+    best_L1_model = LR_L1.best_estimator_
+
+    n_used_features = np.sum(best_L1_model.coef_ != 0, axis=0)
+    print(f"Number of features used (L1): {n_used_features}")
+
+    print("Best parameters (L1):\n", LR_L1.best_params_)
+
+    evaluate_model(best_L1_model, X_train, y_train, X_test, y_test)
+
+    feature_importance(best_L1_model, X_train, y_train, X_test, y_test)
+
+# L2 REGULARIZATION (up to 0)
+def L2_regularization(model, X_train, y_train, X_test, y_test):
+    param_grid = {
+        "C": [1.0],  # Regularization strength
+        "penalty": ["l2"],  # L2 regularization
+        "solver": ["lbfgs"]  # Solver for logistic regression, also possible; saga, ovr
+       # "multi_class": ["ovr"]
+    }
+    LR_L2 = GridSearchCV(
+        model,
+        param_grid,
+        cv=5,
+        scoring='f1_weighted'
+    )
+
+    LR_L2.fit(X_train, y_train)
+
+    best_L2_model = LR_L2.best_estimator_
+    n_used_features_L2 = np.sum(np.any(best_L2_model.coef_ != 0, axis=0))
+    print(f"Number of features used (L2): {n_used_features_L2}")
+
+    print("Best parameters (L2):\n", LR_L2.best_params_)
+
+    evaluate_model(best_L2_model, X_train, y_train, X_test, y_test)
+
+    feature_importance(best_L2_model, X_train, y_train, X_test, y_test)
+
+########################################## Feature Importance ##########################################################
+
+def feature_importance(model, X_train, y_train, X_test, y_test):
+    coefs = model.coef_[0]  # Getting coefficients from the model
+    coef_L1 = pd.DataFrame({
+        'Feature': X_train.columns,  # Using feature names from X_train
+        'Importance': np.abs(coefs)
+    }).sort_values(by='Importance', ascending=False)
+
+    print("\nTop Important Features (L1):")
+    print(coef_L1.head(10))  #
+    """
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x="Importance", y="Feature", data=coef_L1.head(10))
+    plt.title("Top 10 Feature Importances (L2)")
+    plt.tight_layout()
+    
+    file_name = 'L2_features_top_10.png'
+    file_path = os.path.join("output", file_name)
+    plt.savefig(file_path, dpi=300)
+    """
+
+########################################### Model Traininig ############################################################
 
 lr = LogisticRegression(random_state=42, class_weight='balanced', max_iter=10000)
-#lr_multi = LogisticRegression(random_state=42, class_weight='balanced', max_iter=10000, multi_class='multinomial')
 
-L2_regularization(lr, X_train, y_train, X_test, y_test)
+#L2_regularization(lr, X_train, y_train, X_test, y_test)
 # Best C = 1.0
 
+L1_regularization(lr, X_train, y_train, X_test, y_test)
 
 
 end = time.time()
